c9c47287e83b50414c2f61dc8118f9c9d68b1700
Claudivan Barreto <claudivanbarretodasilva@gmail.com>
2018-01-24 10:33:37 -0300

    feat: add fetch stub

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-export const search = () => {};
+export const search = () => {
+  fetch('https://spotify.com');
+};
 export const searchAlbuns = () => {};
 export const searchArtists = () => {};
 export const searchTracks = () => {};
